a9eb36c53e3bce78ec8a840a7d544c62900e0414
robert niyonkuru <robertniyonkuru001@gmail.com>
2026-04-25 15:17:56 +0200

    Update User.java

--- src/main/java/com/inventory/system/entity/User.java
+++ src/main/java/com/inventory/system/entity/User.java
@@ -5,33 +5,29 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
+    
     private String name;
-
-    @Column(unique = true, nullable = false) // This is the fix for "already registered" bugs
+    
+    @Column(unique = true, nullable = false)
+    private String username; // This will be their Reg No or unique ID
+    
     private String email;
-
-    @Column(nullable = false)
     private String password;
 
-    // Default Constructor
     public User() {}
 
     // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
-
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 }
