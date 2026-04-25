89e438179f641ed3606063b5abd69f1edd91655c
robert niyonkuru <robertniyonkuru001@gmail.com>
2026-04-25 15:53:20 +0200

    Update IndexController.java

--- src/main/java/com/inventory/system/controller/IndexController.java
+++ src/main/java/com/inventory/system/controller/IndexController.java
@@ -1,26 +1,15 @@
 package com.inventory.system.controller;
 
-import com.inventory.system.entity.Asset;
-import com.inventory.system.repository.AssetRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class IndexController {
 
-    @Autowired
-    private AssetRepository assetRepository;
-
     @GetMapping("/")
-    public String index(HttpSession session, Model model) {
-        List<Asset> allAssets = assetRepository.findAll();
-        model.addAttribute("totalAssets", allAssets.size());
-        model.addAttribute("assignedCount", allAssets.stream()
-                .filter(a -> a.getStatus() == Asset.Status.ASSIGNED).count());
-        return "index"; // Looks for templates/index.html
+    public String index() {
+        // This ensures that hitting the main URL takes you straight to login
+        // No need for an index.html file!
+        return "redirect:/login";
     }
 }
